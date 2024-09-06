bb8800243ffb208d73d99f9cd1c899965c3823b2
edusousa78 <sousa.ej@gmail.com>
2024-09-06 11:26:12 -0300

    insedidos os comentarios

--- conta-bancaria/src/ContaTerminal.java
+++ conta-bancaria/src/ContaTerminal.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //criando o objeto scaanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
+        
+        // Obter os valores digitados pelo terminal
         System.out.println(" Digite o seu nome: ");
         String nome = scanner.next();
         System.out.println("Digite o Numero da Agencia ex. 000-0: ");
@@ -16,7 +18,7 @@ public class ContaTerminal {
 
         System.out.println("Digite o Saldo da sua da Conta: ");
         double saldoConta = scanner.nextDouble();
-
+        // Exibir as Saídas dos dados após digitados
         System.out.println("Olá " + nome + " Obrigado por criar sua conta em nosso banco! \nSua agencia é: " + agencia + "\nConta: " + numeroConta + "\nSeu Saldo de R$" + saldoConta + " já está disponivel!" );
 
     }
